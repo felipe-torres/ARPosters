b4285b5c90fb77181284d8433c415560be730843
DESKTOP-OP49807\Felipe <lfelipetorresp@gmail.com>
2019-11-29 15:19:44 -0500

    Test some effects, like water droplets and music notes

---
+++ Assets/Scripts/SequenceController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SequenceController : MonoBehaviour
+{
+    public Animator animator;
+    public string triggerParam;
+    public int delaySeconds;
+
+    private void Start()
+    {
+        Invoke("StartSequence", delaySeconds);
+    }
+
+    private void StartSequence()
+    {
+        animator.SetTrigger(triggerParam);
+    }
+}
