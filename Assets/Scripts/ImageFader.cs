342a69b65df7cf2aae1e74d8d5807206610f1a87
Felipe <lfelipetorresp@gmail.com>
2020-02-06 14:51:56 -0600

    Migrated to Vuforia, added 25 poster

---
+++ Assets/Scripts/ImageFader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ImageFader : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+
+    public void ImageRecognized()
+    {
+        spriteRenderer.DOFade(1, 0.5f);
+    }
+
+    public void ImageRecogStopped()
+    {
+        spriteRenderer.DOFade(0, 0);
+    }
+}
--- Assets/Scripts/PosterSpawner.cs
+++ Assets/Scripts/PosterSpawner.cs
@@ -1,34 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR;
-using UnityEngine.XR.ARFoundation;
 
 public class PosterSpawner : MonoBehaviour
-{
-    private ARTrackedImageManager _arTrackedImageManager;
-
-    // Start is called before the first frame update
-    void Arake()
-    {
-        _arTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
-    }
-
-    public void OnEnable()
-    {
-        _arTrackedImageManager.trackedImagesChanged += OnImageChanged;
-    }
-
-    public void OnDisable()
-    {
-        _arTrackedImageManager.trackedImagesChanged -= OnImageChanged;
-    }
-
-    private void OnImageChanged(ARTrackedImagesChangedEventArgs args)
-    {
-        foreach(var trackedImage in args.added)
-        {
-            Debug.Log(trackedImage.name);
-        }
-    }
+{ 
 }
